895dcd8edb8ab0f2d87d5a583d95388ac6a2c63d
Samuel Shen <samuelshen@berkeley.edu>
2018-12-14 13:33:57 -0800

    File structure, currently Internal server error when deployed

---
+++ serverless-image-recognition/handler.js
@@ -0,0 +1,24 @@
+const Twitter = require('twitter');
+const config = require('./config');
+const axios = require('axios');
+
+const twitterClient = new Twitter(config);
+
+/**
+ * @param {*} status
+ * @return {Promise}
+ */
+function statusUpdate(status = 'Hello World!') {
+    return twitterClient.post('statuses/update', { status });
+}
+
+module.exports.tweet = async (event, context) => {
+    await statusUpdate();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Tweeted Hello World',
+        }),
+    };
+};
